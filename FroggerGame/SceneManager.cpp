62de3050b4a59cf7b7e62270b937bd3bb724617a
MijstenHove <mijs.tenhove@hotmail.com>
2022-11-15 18:18:49 +0100

    scene manager

--- FroggerGame/SceneManager.cpp
+++ FroggerGame/SceneManager.cpp
@@ -1,1 +1,21 @@
 #include "SceneManager.h"
+//privete
+SceneManager::SceneManager() {
+
+}
+//public
+SceneManager SceneManager::GetInstanse() {
+
+}
+int SceneManager::AddScene(std::string name, Scene* s) {
+
+}
+Scene* SceneManager::GetScene(std::string name) {
+
+}
+Scene* SceneManager::GetCurrenScene() {
+
+}
+void SceneManager::SetScene(std::string name) {
+
+}
--- FroggerGame/SceneManager.h
+++ FroggerGame/SceneManager.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <unordered_map>
+#include <string>
+#include "Scene.h"
+
 class SceneManager
 {
+private:
+	std::unordered_map<std::string, Scene> scene;
+	Scene* currentScene;
+	static SceneManager* instande;
+
+	SceneManager();
+public:
+	SceneManager GetInstanse();
+	int AddScene(std::string name, Scene* s);
+	Scene* GetScene(std::string name);
+	Scene* GetCurrenScene();
+	void SetScene(std::string name);
+
 };
 
